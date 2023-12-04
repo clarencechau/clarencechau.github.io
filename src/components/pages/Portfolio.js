1113761798918fdb4c470d036c7009eccecb2c32
clarencechau <clarence.comsci@gmail.com>
2023-12-04 13:51:22 -0500

    updated explore page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,6 +1,9 @@
 import { React, useEffect, useRef, useState } from "react";
 import MyButton from "../component/Button";
-import { faArrowTrendUp } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowTrendUp,
+  faEarthAmericas,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Portfolio.css";
 import { motion } from "framer-motion";
@@ -8,22 +11,16 @@ import { motion } from "framer-motion";
 function Portfolio() {
   const projects = [
     {
+      name: "Gungeon",
+      category: "Java",
+      img: "../images/tech/gungeon.png",
+      link: "https://github.com/clarencechau/Gungeon",
+    },
+    {
       name: "UTimetable",
       category: "Java",
       img: "../images/tech/uoft.png",
       link: "https://github.com/clarencechau/UTimetable",
-    },
-    {
-      name: "Three Musketeers",
-      category: "Java",
-      img: "../images/tech/3musketeers.png",
-      link: "https://github.com/clarencechau/ThreeMusketeers",
-    },
-    {
-      name: "Task Application",
-      category: "React Native",
-      img: "../images/tech/taskmanagerapp.png",
-      link: "https://github.com/clarencechau/Task-Application",
     },
     {
       name: "Meepo is You",
@@ -32,20 +29,32 @@ function Portfolio() {
       link: "https://github.com/clarencechau/Meepo-Is-You",
     },
     {
-      name: "Gungeon",
-      category: "Java",
-      img: "../images/tech/gungeon.png",
-      link: "https://github.com/clarencechau/Gungeon",
-    },
-    {
       name: "Picture Manipulation",
       category: "Python",
       img: "../images/tech/toronto.jpg",
       link: "https://github.com/clarencechau/picture-manipulation",
     },
   ];
+
+  const experience = [
+    {
+      name: "Moneris",
+      category: "React, Typescript, Java, SQL",
+      img: "../images/tech/moneris.jpg",
+      link: "https://www.moneris.com/",
+    },
+    {
+      name: "Aireum",
+      category: "React, Javascript, Firebase",
+      img: "../images/tech/aireum.jpg",
+      link: "http://www.aireum.ca/",
+    },
+  ];
+
   const [isMobile, setIsMobile] = useState(false);
-  const [activeProject, setActiveProject] = useState();
+  const [activeHover, setActiveHover] = useState(projects[0]);
+  const [isProject, setIsProject] = useState(true);
+  console.log(activeHover);
 
   const showMobile = () => {
     if (window.innerWidth <= 960) {
@@ -72,13 +81,19 @@ function Portfolio() {
     }
   }, []);
 
-  const handleProjectHover = (project) => {
-    setActiveProject(project);
+  const handleHoverProject = (project) => {
+    setActiveHover(project);
+    setIsProject(true);
+  };
+
+  const handleHoverExperience = (experience) => {
+    setActiveHover(experience);
+    setIsProject(false);
   };
 
   const handleClick = (site) => {
     window.open(site, "_blank");
-    console.log("opened")
+    console.log("opened");
   };
 
   return (
@@ -115,36 +130,87 @@ function Portfolio() {
             <div
               className="project-item"
               key={index}
-              onMouseEnter={() => handleProjectHover(project)}
-              onMouseLeave={() => handleProjectHover(null)}
+              onMouseEnter={() => handleHoverProject(project)}
               onClick={() => handleClick(project.link)}
             >
               <hr />
+
               <div className="project-item-text">
-                <div className="subtext-name">{project.name}</div>
+                <div className="subtext-name">
+                  <i
+                    className="fab fa-github"
+                    style={{
+                      "padding-right": "10px",
+                    }}
+                  ></i>{" "}
+                  {project.name}
+                </div>
                 <div className="subtext-skill">{project.category}</div>
+              </div>
+            </div>
+          ))}
+
+          <div className="experience-title">EXPERIENCE</div>
+          {experience.map((experience, index) => (
+            <div
+              className="project-item"
+              key={index}
+              onMouseEnter={() => handleHoverExperience(experience)}
+              onClick={() => handleClick(experience.link)}
+            >
+              <hr />
+              <div className="project-item-text">
+                <div className="subtext-name">
+                  <FontAwesomeIcon
+                    icon={faEarthAmericas}
+                    style={{
+                      "padding-right": "10px",
+                    }}
+                  />{" "}
+                  {experience.name}
+                </div>
+                <div className="subtext-skill">{experience.category}</div>
               </div>
             </div>
           ))}
         </div>
         {!isMobile && (
           <div className="project-image-container">
-            {projects.map((project, index) => (
-              <motion.img
-                key={index}
-                className="hover-image"
-                src={project.img}
-                alt={project.name}
-                initial={{ opacity: 0 }}
-                animate={{
-                  opacity:
-                    activeProject && project.name === activeProject.name
-                      ? 1
-                      : 0,
-                }}
-                transition={{ duration: 0.5 }}
-              />
-            ))}
+            {isProject
+              ? projects.map((project, index) => (
+                  <motion.img
+                    key={index}
+                    className="hover-image"
+                    src={project.img}
+                    alt={project.name}
+                    initial={{ opacity: 0 }}
+                    animate={{
+                      opacity:
+                        activeHover && project.name === activeHover.name
+                          ? 1
+                          : 0,
+                    }}
+                    transition={{ duration: 0.5 }}
+                  />
+                ))
+              : experience.map((experience, index) => (
+                  <div>
+                    <motion.img
+                      key={index}
+                      className="hover-image"
+                      src={experience.img}
+                      alt={experience.name}
+                      initial={{ opacity: 0 }}
+                      animate={{
+                        opacity:
+                          activeHover && experience.name === activeHover.name
+                            ? 1
+                            : 0,
+                      }}
+                      transition={{ duration: 0.5 }}
+                    />
+                  </div>
+                ))}
           </div>
         )}
       </div>
