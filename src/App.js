39d55d40b5e736e52547ef520f1cd9663b81320f
clarencechau <clarence.comsci@gmail.com>
2024-02-13 19:32:20 -0500

    changed to hashrouting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import AnimatedRoutes from "./components/component/AnimatedRoutes";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <AnimatedRoutes />
-    </Router>
+    </HashRouter>
   );
 }
 
