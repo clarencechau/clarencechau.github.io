8628863cf1c4ea6df646ccb67aab395c31538548
clarencechau <clarence.comsci@gmail.com>
2024-12-09 14:00:31 -0500

    desktop portfolio done

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -10,6 +10,12 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
 import { Grid2 } from "@mui/material";
+import Link from "@mui/icons-material/Link";
+import Place from "@mui/icons-material/Place";
+import Work from "@mui/icons-material/Work";
+import ThumbUp from "@mui/icons-material/ThumbUp";
+import VideoViews from "../../api/VideoViews";
+import VideoLikes from "../../api/VideoLikes";
 
 function Portfolio() {
   const projects = [
@@ -17,16 +23,17 @@ function Portfolio() {
       name: "GoTourney (Current Project)",
       category: "NextJS, MongoDB",
       img: "../images/badminton-image.jpg",
+      link: "#",
       description:
-        "As a national level badminton player who plays many types of tournaments, I am developing a website that makes life easier for players and tournament hosts to create and advertise local tournaments.",
+        "Developing a website that makes life easier for players and tournament hosts to create and advertise local tournaments.",
     },
     {
       name: "Gungeon",
-      category: "Java",
+      category: "Java, Greenfoot",
       img: "../images/tech/gungeon.png",
       link: "https://github.com/clarencechau/Gungeon",
       description:
-        "Gungeon is a remake of the classic Gungeon. The player travels through chambers/rooms, shooting enemies and avoiding traps to beat the game with multiple save files and checkpoints. It contains a store feature to allow the player to purchase stronger weapons, health refills, and ammo. Enter the Gungeon!",
+        "Travel through chambers/rooms, shooting enemies and avoiding traps to beat the game with multiple save files and checkpoints.",
     },
     {
       name: "UTimetable",
@@ -34,7 +41,7 @@ function Portfolio() {
       img: "../images/tech/uoft.png",
       link: "https://github.com/clarencechau/UTimetable",
       description:
-        "Application allowing University of Toronto students to compare timetables with their friends in order to find mutual free timeslots. You can use this program to find time to study with friends, create meetings for school clubs, or anything to do with collaborating with other students.",
+        "Application allowing University of Toronto students to compare timetables with their friends in order to find mutual free timeslots.",
     },
     {
       name: "Meepo is You",
@@ -42,7 +49,7 @@ function Portfolio() {
       img: "../images/tech/meepoisyou.png",
       link: "https://github.com/clarencechau/Meepo-Is-You",
       description:
-        'A project in Python based on a popular puzzle game "Baba is You". The goal of the game is to get Meepo to the finish flag by going through different obstacles and rooms, by moving word blocks that affect the rules of the game.',
+        "Get Meepo to the finish flag by going through different obstacles and rooms, by moving word blocks that affect the rules of the game.",
     },
     {
       name: "Picture Manipulation",
@@ -50,27 +57,69 @@ function Portfolio() {
       img: "../images/tech/toronto.jpg",
       link: "https://github.com/clarencechau/picture-manipulation",
       description:
-        "A project I made with the purpose of recreating some of the basic features Adobe Photoshop provides, such as mirroring photos, changing saturation, and manipulating the loss level of the picture after the transformation has been completed.",
+        "Image editing applicataion allowing mirroring photos, changing saturation, and manipulating the loss levels after transformations.",
     },
   ];
 
   const experience = [
     {
-      name: "Moneris",
+      name: "Data Engineer",
+      category: "Python, SQL, Spark, MinIO, Cloud",
+      img: "../images/tech/scotiabank.png",
+      link: "http://www.aireum.ca/",
+      description:
+        "Incoming data engineer intern on the Canadian Banking team, focusing on customer insight and data analytics.",
+    },
+    {
+      name: "Software Engineer",
       category: "React, Typescript, Java, SQL",
       img: "../images/tech/moneris.jpg",
       link: "https://www.moneris.com/",
+      description:
+        "Collaborated with a cross-functional team to architect and rebuild a merchant chargebacks application, improving efficiency, user experience, and code modernity.",
     },
     {
-      name: "Aireum",
+      name: "Full Stack Developer",
       category: "React, Javascript, Firebase",
-      img: "../images/tech/aireum.jpg",
+      img: "../images/tech/aireum2.png",
       link: "http://www.aireum.ca/",
+      description:
+        "Engineered a full-stack administrative management system, enabling the digital showcasing and publication of Canadian national archive material.",
+    },
+  ];
+
+  const videos = [
+    {
+      name: "My First NATIONAL Title",
+      img: "../images/tech/youtube1.jpg",
+      link: "https://youtu.be/obTF6hIhac8",
+      description:
+        "I traveled across Canada to Moncton, New Brunswick, vlogging the journey of obtaining my first Canadian national title.",
+    },
+    {
+      name: "Can We Win Nationals AGAIN??",
+      img: "../images/tech/youtube2.webp",
+      link: "https://youtu.be/470llxjdbwA",
+      description:
+        "After obtaining my first national title, I flew to Quebec City to try and win my second back-to-back men's doubles national gold.",
     },
   ];
 
   const [isMobile, setIsMobile] = useState(false);
   const [activeHover, setActiveHover] = useState(projects[0]);
+
+  const getYouTubeVideoId = (url) => {
+    try {
+      const urlObj = new URL(url); // Parse the URL
+      if (urlObj.hostname === "youtu.be") {
+        return urlObj.pathname.slice(1); // Extract the path and remove the leading '/'
+      }
+      throw new Error("Invalid YouTube short URL");
+    } catch (error) {
+      console.error("Error extracting video ID:", error.message);
+      return null;
+    }
+  };
 
   const showMobile = () => {
     if (window.innerWidth <= 960) {
@@ -127,34 +176,92 @@ function Portfolio() {
       <div className="portfolio-container">
         <div className="projects-container">
           <div className="projects">
-            <div className="title">Projects</div>
+            <h2 className="title">Experience</h2>
+
+            <div className="cards-section">
+              <Grid2 container spacing={2} justifyContent="center">
+                {experience.map((experience, index) => (
+                  <Grid2 item xs={6} key={index}>
+                    <Card sx={{ maxWidth: 360 }}>
+                      <CardActionArea href={experience.link} target="_blank">
+                        <CardMedia
+                          component="img"
+                          height="200"
+                          image={experience.img}
+                          alt={experience.name}
+                          sx={{ scale: 1.01 }}
+                        />
+                        <CardContent sx={{ minHeight: 225 }} className="card">
+                          <h2 className="card-header">{experience.name}</h2>
+                          <Typography sx={{ color: "grey" }}>
+                            {experience.category}
+                          </Typography>
+                          <p>{experience.description}</p>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  </Grid2>
+                ))}
+              </Grid2>
+            </div>
+            <h2 className="title">Projects</h2>
             <div className="cards-section">
               <Grid2 container spacing={2} justifyContent="center">
                 {projects.map((project, index) => (
                   <Grid2 item xs={6} key={index}>
-                    <Card sx={{ maxWidth: 345 }}>
+                    <Card sx={{ maxWidth: 360 }}>
                       <CardActionArea href={project.link} target="_blank">
                         <CardMedia
                           component="img"
-                          height="250"
+                          height="200"
                           image={project.img}
                           alt={project.name}
                         />
-                        <CardContent sx={{ minHeight: 175 }}>
-                          <Typography
-                            gutte
-                            rBottom
-                            variant="h5"
-                            component="div"
-                          >
-                            {project.name}
+                        <CardContent sx={{ minHeight: 210 }}>
+                          <h2 className="card-header"> {project.name}</h2>
+                          <Typography sx={{ color: "grey" }}>
+                            {project.category}
                           </Typography>
-                          <Typography
-                            variant="body2"
-                            sx={{ color: "text.secondary" }}
-                          >
-                            {project.description}
-                          </Typography>
+
+                          <p>{project.description}</p>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  </Grid2>
+                ))}
+              </Grid2>
+            </div>
+            <h2 className="title">Youtube Videos</h2>
+            <div className="cards-section">
+              <Grid2 container spacing={2} justifyContent="center">
+                {videos.map((video, index) => (
+                  <Grid2 item xs={6} key={index}>
+                    <Card sx={{ maxWidth: 360 }}>
+                      <CardActionArea href={video.link} target="_blank">
+                        <CardMedia
+                          component="img"
+                          height="200"
+                          image={video.img}
+                          alt={video.name}
+                        />
+
+                        <CardContent sx={{ minHeight: 100 }}>
+                          <h4 className="card-header"> {video.name}</h4>
+                          <div className="video-stats">
+                            <Typography sx={{ color: "grey" }}>
+                              <VideoViews
+                                videoId={getYouTubeVideoId(video.link)}
+                              />
+                            </Typography>
+                            <Typography sx={{ color: "grey", display: "flex" }}>
+                              <ThumbUp sx={{ marginTop: 2, paddingRight: 1}} />
+                              <VideoLikes
+                                videoId={getYouTubeVideoId(video.link)}
+                              />
+                            </Typography>
+                          </div>
+
+                          <p>{video.description}</p>
                         </CardContent>
                       </CardActionArea>
                     </Card>
@@ -166,7 +273,80 @@ function Portfolio() {
         </div>
         <div className="profile-container">
           <div className="profile">
-            <img className="headshot-square" src="../images/headshot.jpg" alt="" />
+            <div className="portfolio-tag">
+              &lt; &nbsp; portfolio &nbsp;/ &nbsp;&gt;
+            </div>
+
+            <img
+              className="headshot-square"
+              src="../images/headshot.jpg"
+              alt=""
+            />
+            <div className="github-name">Clarence Chau</div>
+            <a
+              href="https://github.com/clarencechau"
+              target="_blank"
+              className="github-link"
+            >
+              @clarencechau
+            </a>
+            <a
+              href="https://www.clarencechau.com"
+              target="_blank"
+              className="website-link"
+            >
+              <Link className="link-icon" />
+              <div className="link-name">https://clarencechau.com</div>
+            </a>
+            <div className="current-place">
+              <Place className="place-icon" />
+              <div className="place-name">Toronto</div>
+            </div>
+            <div className="current-work">
+              <Work className="work-icon" />
+              <div className="work-name">
+                Incoming Data Engineer @ Scotiabank
+              </div>
+            </div>
+
+            <div className="icons-section">
+              <a
+                className="social-icon-link-black linkedin"
+                href="https://ca.linkedin.com/in/clarence-chau-3a827b1b7"
+                target="_blank"
+                aria-label="LinkedIn"
+                rel="noreferrer noopener"
+              >
+                <i className="fab fa-linkedin"></i>
+              </a>
+              <a
+                className="social-icon-link-black github"
+                href="https://github.com/clarencechau"
+                target="_blank"
+                aria-label="GitHub"
+                rel="noreferrer noopener"
+              >
+                <i className="fab fa-github"></i>
+              </a>
+              <a
+                className="social-icon-link-black youtube"
+                href="https://www.youtube.com/@clarencechauu"
+                target="_blank"
+                aria-label="Youtube"
+                rel="noreferrer noopener"
+              >
+                <i className="fab fa-youtube"></i>
+              </a>
+              <a
+                className="social-icon-link-black instagram"
+                href="https://www.instagram.com/clarencechauu/"
+                target="_blank"
+                aria-label="Instagram"
+                rel="noreferrer noopener"
+              >
+                <i className="fab fa-instagram"></i>
+              </a>
+            </div>
           </div>
         </div>
       </div>
